023023e09757425b7890842db9134ee3e4aca33f
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-11-16 13:16:28 +0100

    Update user.vixenin.nix
    Added extragroup "gamemode" required since gamemode 1.8 to change cpu governor

--- modules/user.vixenin.nix
+++ modules/user.vixenin.nix
@@ -11,7 +11,7 @@
     packages = with pkgs; [ kdePackages.kate ];
   };
 
-  # Timezone and locale settings
+  # Timezone and locale Settings
   time.timeZone = "Europe/Berlin";
   i18n = {
     defaultLocale = "en_US.UTF-8";
