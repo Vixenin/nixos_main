bdbceb94add5a54354d8d8c0a28ff9d85bfb3e40
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-11-30 22:44:40 +0100

    Update vr.nix
    WIP VR with WiVern

--- modules/vr.nix
+++ modules/vr.nix
@@ -10,21 +10,7 @@
         };
     };
 
-    services.monado = {
-        enable = true;
-        defaultRuntime = true; # Register as default openxr runtime
-    };
-
-    systemd.user.services.monado.environment = {
-        STEAMVR_LH_ENABLE = "1";
-        XRT_COMPOSITOR_COMPUTE = "1";
-    };
-
-    environment.systemPackages = with pkgs; [
-        envision
-    ];
-
-    # Install wivrn
+    # Install wivrn :3
     systemd.services.flatpak-wivrn-install = {
         wantedBy = [ "multi-user.target" ];
         after = [ "flatpak-repo.service" ];
