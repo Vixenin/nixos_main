287b5722406592104324265c6376355898d49446
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-11-30 09:36:52 +0100

    Update boot.nix
    Nvidia tweaks

--- modules/boot.nix
+++ modules/boot.nix
@@ -8,11 +8,15 @@
       efi.canTouchEfiVariables = true;
     };
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    #kernelPackages = pkgs.linuxPackages_latest;
+
+    # Fuck you ngreedia with your shit drivers :3
+    kernelPackages = pkgs.linuxPackages_6_11;
 
     # Nvidia wayland tweaks & ghost monitor fix
     kernelParams = [
       "nvidia-drm.modeset=1"
+      "nouveau.modeset=0"
       "nvidia-drm.fbdev=1"
       "ipv6.disable=1"
     ];
