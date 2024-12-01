7bf8a8d29767c9d5d383860ba586a7ac43d0baa6
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-01 04:09:56 +0100

    Update vr.nix
    Added nssmdns4 for Avahi

--- modules/vr.nix
+++ modules/vr.nix
@@ -4,6 +4,7 @@
     # Avahi for vr | wivrn
     services.avahi = {
         enable = true;
+        nssmdns4 = true;
         publish = {
             enable = true;
             userServices = true;
