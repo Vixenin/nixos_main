f13c5ff752bafe20f9517a4ed80ac177db78f9fb
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-03 03:14:40 +0100

    Update nvidia.nix
    Removed deprecated dependencies for Vulkan

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -24,24 +24,16 @@
     nvidiaSettings = true;
 
     # Nvidia custom driver version
-    package = let
-      nvidiaDrivers = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "565.57.01";
-        sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
-        sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
-        openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
-        settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
-        persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
-        patchesOpen = [
-          ./nvidia.patch
-        ];
-      };
-      
-    # Temporary workaround for custom vulkan headers
-    in nvidiaDrivers // {
-      settings = nvidiaDrivers.settings.overrideAttrs (oldAttrs: {
-        buildInputs = oldAttrs.buildInputs ++ [ pkgs.vulkan-headers ];
-      });
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+	    version = "565.57.01";
+      sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+      sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
+      openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+      settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+      persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
+      patchesOpen = [
+        ./nvidia.patch
+      ];
     };
   };
 
