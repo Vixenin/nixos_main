cd40984e1b644f8266d356183cbd477c4ebd9a7f
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-04 19:29:48 +0100

    Update environment.nix
    Simplified code and fixed permissions

--- modules/environment.nix
+++ modules/environment.nix
@@ -19,7 +19,7 @@
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
 
     # Drm kernel driver 'nvidia-drm' fix
-    VK_DRIVER_FILES=/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json;
+    VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
 
     # Nvidia 10gb shader cache
     __GL_SHADER_DISK_CACHE = "1";
@@ -39,37 +39,39 @@
   hardware.xone.enable = true;
 
   # Dconf values logic
-  system.activationScripts.dconfSetup = {
-    text = let
-      dconfSettings = ''
+  system.userActivationScripts.setupDconf = {
+    text = ''
+      dconfSettings=$(cat <<EOF
+
         # Disable legacy tray for appindicator extension / xwayland fix
         dconf write /org/gnome/shell/extensions/appindicator/legacy-tray-enabled false
 
         # Disable gnome tablet input management
         dconf write /org/gnome/settings-daemon/plugins/peripherals/tablets/active false
-      '';
-      mangohudSettings = ''
+  EOF
+      )
+
+      # Dconf setup
+      mkdir -p ~/.config/dconf
+      echo "$dconfSettings" > ~/.config/dconf/user-dconf
+    '';
+  };
+
+  system.userActivationScripts.setupMangoHud = {
+    text = ''
+      mangohudSettings=$(cat <<EOF
         fps_limit=162
         fps_only=1
         font_size=20
         position=top-left
         text_color=FF69B4
         background_alpha=0
-      '';
-    in ''
-      for user in /home/*; do
-        if [ -d "$user" ]; then
-          # Dconf setup
-          mkdir -p "$user/.config/dconf"
-          echo "${dconfSettings}" > "$user/.config/dconf/user-dconf"
-          chown $(basename "$user") "$user/.config/dconf/user-dconf"
+  EOF
+      )
 
-          # Mangohud setup
-          mkdir -p "$user/.config/MangoHud"
-          echo "${mangohudSettings}" > "$user/.config/MangoHud/MangoHud.conf"
-          chown $(basename "$user") "$user/.config/MangoHud/MangoHud.conf"
-        fi
-      done
+      # Mangohud setup
+      mkdir -p ~/.config/MangoHud
+      echo "$mangohudSettings" > ~/.config/MangoHud/MangoHud.conf
     '';
   };
 }
