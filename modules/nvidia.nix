364fd78e20e65c010d1e7435b6b25887f110e564
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-06 00:08:00 +0100

    Update nvidia.nix
    Reverted version for now (some games refuse to detect the gpu)

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -28,11 +28,20 @@
 
     # Nvidia custom driver version
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "565.77";
-      sha256_64bit = "sha256-CnqnQsRrzzTXZpgkAtF7PbH9s7wbiTRNcM0SPByzFHw=";
-      openSha256 = lib.fakeSha256;
-      settingsSha256 = "sha256-VUetj3LlOSz/LB+DDfMCN34uA4bNTTpjDrb6C6Iwukk=";
-      persistencedSha256 = "sha256-bFtFhYHzRn6GqwWNWKYLxuRcRFhLTRbJviBNEt8Xss=";
+      version = "565.57.01";
+      sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+      sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
+      openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+      settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+      persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
+      patches = [
+        (pkgs.fetchpatch {
+          # Kernel 6.12 compatibility
+          # https://forums.developer.nvidia.com/t/patch-for-565-57-01-linux-kernel-6-12/313260
+          url = "https://github.com/Binary-Eater/open-gpu-kernel-modules/commit/8ac26d3c66ea88b0f80504bdd1e907658b41609d.patch";
+          hash = "sha256-+SfIu3uYNQCf/KXhv4PWvruTVKQSh4bgU1moePhe57U=";
+        })
+      ];
     };
   };
 
