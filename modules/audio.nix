bb20759a1a7bc3c0b8c7163e7d686cebfb5153f5
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-05 23:29:33 +0100

    Update audio.nix
    Simplified and removed redundancies

--- modules/audio.nix
+++ modules/audio.nix
@@ -2,24 +2,17 @@
 
 {
   # Standard audio implementation
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-
   services.pipewire = {
     enable = true;
+    pulse.enable = true;
     alsa = { 
       enable = true; 
       support32Bit = true; 
     };
-
-    pulse.enable = true;
-    wireplumber.enable = true;
-    jack.enable = true;
+    
+    wireplumber.extraConfig = {
+      "dont-switch-device-profiles"."wireplumber.settings"."bluetooth.autoswitch-to-headset-profile" = false;
+    };
   };
-
-  # Alsa default sound card
-  environment.etc."asound.conf".text = ''
-    defaults.pcm.card 1
-    defaults.ctl.card 1
-  '';
 }
