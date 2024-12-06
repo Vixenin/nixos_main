1f9307874163e0275fe6b86bcdc775a890774474
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-07 00:33:19 +0100

    Update environment.nix

--- modules/environment.nix
+++ modules/environment.nix
@@ -23,6 +23,9 @@
     __GL_SHADER_DISK_CACHE_PATH = "/home/vixenin/.nv_shader_cache";
     __GL_SHADER_DISK_CACHE_SIZE = "10737418240";
 
+    # Drm kernel driver 'nvidia-drm' fix
+    VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+
     # Fix vulkan layers path
     VK_LAYER_PATH = "/run/opengl-driver/share/vulkan/implicit_layer.d";
     VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
