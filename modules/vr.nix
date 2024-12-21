eab1824a374cc2ba7fdd3b92eaa46640b52c56f5
Vixenin <48571695+Vixenin@users.noreply.github.com>
2024-12-21 18:34:21 +0100

    Update vr.nix

--- modules/vr.nix
+++ modules/vr.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
 
 {
-    # Avahi for vr | wivrn
+    environment.systemPackages = [
+        pkgs.monado-vulkan-layers # Monado vulkan layers for vr | wivrn
+        pkgs.wlx-overlay-s # Overlay for vr | wivrn 
+        pkgs.android-tools # Adb for reverse tcp | wivrn
+    ];
+
+    # Monado vulkan layers for vr | wivrn (https://github.com/PassiveLemon/lemonake/issues/9#issuecomment-2525188053)
+    hardware.graphics.extraPackages = with pkgs; [monado-vulkan-layers];
+
+    # Avahi service for vr | wivrn
     services.avahi = {
         enable = true;
         nssmdns4 = true;
@@ -17,24 +26,15 @@
             wivrnConfig=$(cat <<EOF
                 {
                     "scale": 0.4,
-                    "bitrate": 80000000,
+                    "bitrate": 50000000,
                     "encoders": [
                         {
                             "encoder": "nvenc",
                             "codec": "h264",
                             "width": 0.5,
-                            "height": 0.25,
+                            "height": 1.0,
                             "offset_x": 0.0,
                             "offset_y": 0.0,
-                            "group": 0
-                        },
-                        {
-                            "encoder": "nvenc",
-                            "codec": "h264",
-                            "width": 0.5,
-                            "height": 0.75,
-                            "offset_x": 0.0,
-                            "offset_y": 0.25,
                             "group": 0
                         },
                         {
@@ -56,37 +56,5 @@
             mkdir -p ~/.var/app/io.github.wivrn.wivrn/config/wivrn
             echo "$wivrnConfig" > ~/.var/app/io.github.wivrn.wivrn/config/wivrn/config.json
         '';
-    };
-
-    # Install wlx-overlay-s
-    environment.systemPackages = [
-        pkgs.wlx-overlay-s
-    ];
-
-    # Reverse adb tether service for wivrn
-    systemd.services.adb-reverse = {
-        description = "Maintain ADB Reverse for Wivrn";
-        wantedBy = [ "multi-user.target" ];
-        after = [ "network-online.target" ];
-        wants = [ "network-online.target" ];
-        path = [ pkgs.android-tools ];
-        serviceConfig = {
-            ExecStart = "${pkgs.android-tools}/bin/adb reverse tcp:9757 tcp:9757";
-            Restart = "always";
-            RestartSec = 5;
-            StandardOutput = "journal";
-            StandardError = "journal";
-        };
-    };
-
-    # Wivrn launch Service (manual trigger)
-    systemd.services.wivrn-launch = {
-        description = "Launch Wivrn on Android Device";
-        path = [ pkgs.android-tools ];
-        serviceConfig = {
-            ExecStart = "${pkgs.android-tools}/bin/adb shell am start -a android.intent.action.VIEW -d 'wivrn+tcp://localhost' org.meumeu.wivrn.github";
-            StandardOutput = "journal";
-            StandardError = "journal";
-        };
     };
 }
