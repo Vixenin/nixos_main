ca63a8642e3a2a063bcaf13906ae73d444941abc
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-03-20 17:59:17 +0100

    Update user.vixenin.nix
    Update to english international / eu

--- modules/user.vixenin.nix
+++ modules/user.vixenin.nix
@@ -6,26 +6,26 @@
     isNormalUser = true;
     description = "Vixenin";
     
-    # Extragroup "gamemode" required since gamemode 1.8 to change cpu governor
+    # Extra group "gamemode" required since gamemode 1.8 to change CPU governor
     extraGroups = [ "networkmanager" "wheel" "gamemode" ];
   };
 
-  # Set time zone.
+  # Set time zone
   time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
+  # Set internationalization properties
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 
   # Configure keymap in X11
