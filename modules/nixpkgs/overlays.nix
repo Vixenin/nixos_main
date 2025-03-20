65a05dca7f99477a806fee6c729d765edc8fbba2
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-03-20 18:00:30 +0100

    Update overlays.nix
    Removed redundancy

--- modules/nixpkgs/overlays.nix
+++ modules/nixpkgs/overlays.nix
@@ -7,9 +7,6 @@
         r2modman = import ./r2modman/package.nix {
           inherit (super) lib stdenv yarn fetchYarnDeps fixup-yarn-lock nodejs electron fetchFromGitHub nix-update-script makeWrapper makeDesktopItem copyDesktopItems;
         };
-        vintagestory = import ./vintagestory/package.nix {
-          inherit (super) lib stdenv fetchurl makeWrapper makeDesktopItem copyDesktopItems xorg gtk2 sqlite openal cairo libGLU SDL2 freealut libglvnd pipewire libpulseaudio dotnet-runtime_8;
-        };
       };
     })
   ];
