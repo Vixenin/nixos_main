337e410e7bc66f55af9054844c2be1fa2c9c301e
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-04-04 19:16:54 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,21 @@
       ./modules/vr.nix
     ];
 
+  # Setup the storage
+  fileSystems."/mnt/Mango" = {
+    label = "Mango";
+    device = "/dev/disk/by-uuid/5aace8e8-d6f4-489f-8e29-ee07e5c51b91";
+    fsType = "ext4";
+    options = [ "nofail" "rw" "noatime" ];
+  };
+
+  fileSystems."/mnt/Backup" = {
+    label = "Backup";
+    device = "/dev/disk/by-uuid/c515297a-7147-4c84-92fe-24939a787555";
+    fsType = "ext4";
+    options = [ "nofail" "rw" "noatime" ];
+  };
+
   # System State Version
   system.stateVersion = "24.11";
 }
