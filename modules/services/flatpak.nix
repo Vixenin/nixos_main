47cc27d4cbacd6802db343b36bd12d7cb1fe952f
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-04-12 09:38:42 +0200

    Update flatpak.nix

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -3,4 +3,31 @@
 {
   # Install flatpak & flatbub repository
   services.flatpak.enable = true;
+
+  # Fix flatpaks dependencies
+  programs.nix-ld = {
+    enable = true;
+
+    libraries = with pkgs; [
+      zlib
+      zstd
+      stdenv.cc.cc
+      stdenv.cc.cc.lib
+      udev
+      dbus
+      mesa
+      libglvnd
+      curl
+      openssl
+      attr
+      libssh
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+    ];
+  };
 }
