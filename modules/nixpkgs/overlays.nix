6a383e3d7bd4716abde23b4ae0aa4e01721a1d92
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-04-18 07:39:04 +0200

    Update overlays.nix
    Removed redundancies, implement LACT

--- modules/nixpkgs/overlays.nix
+++ modules/nixpkgs/overlays.nix
@@ -4,10 +4,28 @@
   nixpkgs.overlays = [
     (self: super: {
       vPack = {
-        r2modman = import ./r2modman/package.nix {
-          inherit (super) lib stdenv yarn fetchYarnDeps fixup-yarn-lock nodejs electron fetchFromGitHub nix-update-script makeWrapper makeDesktopItem copyDesktopItems;
-        };
+        lact = self.callPackage ./lact/package.nix { };
       };
     })
   ];
+
+  #enable lact and lactd.service
+
+  environment.systemPackages = [ pkgs.vPack.lact ];
+
+  systemd.services.lactd = {
+    description = "LACT Daemon";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.vPack.lact}/bin/lact daemon";
+      Type = "simple";
+      # Run as root since we need direct hardware access
+      User = "root";
+      Group = "root";
+      Restart = "on-failure";
+      RestartSec = "5";
+      Environment = "LD_LIBRARY_PATH=/run/opengl-driver/lib";
+    };
+  };
 }
