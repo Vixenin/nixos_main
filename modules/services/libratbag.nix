80b0ddd043a35a8c7cb3d8fb2323394b65f62699
Vixenin <48571695+Vixenin@users.noreply.github.com>
2025-05-21 18:15:04 +0200

    Update libratbag.nix

--- modules/services/libratbag.nix
+++ modules/services/libratbag.nix
@@ -1,18 +1,11 @@
 { config, pkgs, ... }:
 
-let
-  unstable = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-  }) {
-    config.allowUnfree = true;
-  };
-in
 {
   # Libratbag daemon
   services.ratbagd.enable = true;
 
   # Piper
   environment.systemPackages = with pkgs; [
-    unstable.piper
+    piper
   ];
 }
